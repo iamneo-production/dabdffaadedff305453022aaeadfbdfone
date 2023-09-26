31f0201f642b6baa12bd9a1b77fc390759265bae
Iamneo <techadmin@iamneo.ai>
2023-09-26 16:59:56 +0000

    Backup Commit

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -7,8 +7,8 @@ app.use(express.json());
 // Sample data (in-memory storage)
 const items = [
   { id: 1, name: 'Item 1' },
-  { id: 2, name: 'Item 2' },
-  { id: 3, name: 'Item 3' },
+  //{ id: 2, name: 'Item 2' },
+  // { id: 3, name: 'Item 3' },
 ];
 
 // Route to get a list of items
